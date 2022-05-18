94b665f172cfac9ae65a35679262c496c56c1cde
naeem123650 <naeemmemon60650@gmail.com>
2022-05-18 16:33:21 +0530

    change button test.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,5 +2,5 @@ import React from 'react'
 import styles from '../styles/components/Button.module.css'
 
 export function Button() {
-  return <input type='submit' value='new note' className={styles.button} />
+  return <input type='submit' value='add new note Please' className={styles.button} />
 }
